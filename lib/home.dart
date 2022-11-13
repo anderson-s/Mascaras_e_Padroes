2628fbe62f47b3d14f8e10565fc54bd33d707ba8
Andeson Silva <andsilva607@gmail.com>
2022-11-13 17:46:36 -0300

    Adicionando padrões e mascara de real

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _cpf = TextEditingController();
+  final TextEditingController _cnpj = TextEditingController();
+  final TextEditingController _cep = TextEditingController();
+  final TextEditingController _telefone = TextEditingController();
+  final TextEditingController _data = TextEditingController();
+  final TextEditingController _dinheiro = TextEditingController();
+  @override
+  void dispose() {
+    _cpf.dispose();
+    _cnpj.dispose();
+    _cep.dispose();
+    _telefone.dispose();
+    _data.dispose();
+    _dinheiro.dispose();
+    super.dispose();
+  }
+
+  _recuperar() {
+    String moeda = _dinheiro.text.toString();
+    moeda = moeda.replaceAll(".", "");
+    moeda = moeda.replaceAll(".", ",");
+    print(double.parse(UtilBrasilFields.removerSimboloMoeda(
+        moeda))); //Transformando o valores apenas em double
+    print(UtilBrasilFields.obterReal(double.parse(
+        UtilBrasilFields.removerSimboloMoeda(
+            moeda)))); //Deixando a formatação novamente como antes
+  }
+
+  _padroes() {
+    //Estados
+    print("###########Estados:##########");
+    for (int i = 0; i < Estados.listaEstados.length; i++) {
+      print(Estados.listaEstados[i]);
+    }
+
+    //Meses
+    print("##########Meses###########");
+    for (int i = 0; i < Meses.listaMeses.length; i++) {
+      print(Meses.listaMeses[i]);
+    }
+
+    //Regioes
+    print("##########Regiões##########");
+    for (int i = 0; i < Regioes.listaRegioes.length; i++) {
+      print(Regioes.listaRegioes[i]);
+    }
+    //Semana
+    print("##########Semana##########");
+    for (int i = 0; i < Semana.listaDiasSemana.length; i++) {
+      print(Semana.listaDiasSemana[i]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,39 +71,79 @@ class _HomeState extends State<Home> {
         title: const Text("Máscaras e Padrões"),
         centerTitle: true,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            InputTextFormField(
-              tipo: TextInputType.number,
-              formato: [
-                FilteringTextInputFormatter.digitsOnly,
-                CpfInputFormatter()
-              ],
-            ),
-            InputTextFormField(
-              tipo: TextInputType.number,
-              formato: [
-                FilteringTextInputFormatter.digitsOnly,
-                CnpjInputFormatter()
-              ],
-            ),
-            InputTextFormField(
-              tipo: TextInputType.number,
-              formato: [
-                FilteringTextInputFormatter.digitsOnly,
-                CepInputFormatter()
-              ],
-            ),
-            InputTextFormField(
-              tipo: TextInputType.number,
-              formato: [
-                FilteringTextInputFormatter.digitsOnly,
-                CnpjInputFormatter()
-              ],
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              InputTextFormField(
+                controller: _cpf,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CpfInputFormatter()
+                ],
+                texto: "CPF",
+              ),
+              InputTextFormField(
+                controller: _cnpj,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CnpjInputFormatter()
+                ],
+                texto: "CNPJ",
+              ),
+              InputTextFormField(
+                controller: _cep,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CepInputFormatter()
+                ],
+                texto: "CEP",
+              ),
+              InputTextFormField(
+                controller: _telefone,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  TelefoneInputFormatter()
+                ],
+                texto: "Telefone",
+              ),
+              InputTextFormField(
+                controller: _data,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  DataInputFormatter()
+                ],
+                texto: "Data",
+              ),
+              InputTextFormField(
+                controller: _dinheiro,
+                tipo: TextInputType.number,
+                formato: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  RealInputFormatter(moeda: true)
+                ],
+                texto: "Real",
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_dinheiro.text.isNotEmpty) {
+                      _recuperar();
+                    }
+                    _padroes();
+                  },
+                  child: const Text("Recuperar"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/input_text.dart
+++ lib/input_text.dart
@@ -4,12 +4,23 @@ import 'package:flutter/services.dart';
 class InputTextFormField extends StatelessWidget {
   final TextInputType tipo;
   final List<TextInputFormatter> formato;
-  const InputTextFormField(
-      {super.key, required this.tipo, required this.formato});
+  final String texto;
+  final TextEditingController controller;
+  const InputTextFormField({
+    super.key,
+    required this.tipo,
+    required this.formato,
+    required this.texto,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: texto,
+      ),
       keyboardType: tipo,
       inputFormatters: formato,
     );
